65e97b0234d51abba55cb4f96fa02ce62279bcc3
skyme32 <marcosmejiasg@gmail.com>
2020-10-19 21:53:00 +0200

    Unit 3

--- app/src/main/java/com/skyme32/diceroller/MainActivity.kt
+++ app/src/main/java/com/skyme32/diceroller/MainActivity.kt
@@ -6,8 +6,8 @@
 package com.skyme32.diceroller
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -16,25 +16,42 @@ import androidx.appcompat.app.AppCompatActivity
  */
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage : ImageView
+    lateinit var linearView: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val resultTextView: TextView = findViewById(R.id.textView)
-        val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener { rollDice() }
+        diceImage = findViewById(R.id.dice_image)
+        linearView = findViewById(R.id.view)
+        linearView.setOnClickListener {
+            rollDice(diceImage)
+        }
     }
 
     /**
      * Roll the dice and update the screen with the result.
      */
-    private fun rollDice() {
+    private fun rollDice(diceImage: ImageView) {
+        diceImage.setImageResource(getRandomDiceImage())
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun getRandomDiceImage() : Int {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
-        val diceRoll = dice.roll()
 
         // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        return when (dice.roll()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
     }
 }
