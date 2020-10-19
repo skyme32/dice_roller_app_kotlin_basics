430afa6252a1ce55521e9ba97be9fc76da2f6af7
skyme32 <marcosmejiasg@gmail.com>
2020-10-19 22:13:28 +0200

    change the homework

--- app/src/main/java/com/skyme32/diceroller/MainActivity.kt
+++ app/src/main/java/com/skyme32/diceroller/MainActivity.kt
@@ -8,6 +8,7 @@ package com.skyme32.diceroller
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -16,17 +17,26 @@ import androidx.appcompat.app.AppCompatActivity
  */
 class MainActivity : AppCompatActivity() {
 
-    lateinit var diceImage : ImageView
-    lateinit var linearView: View
+    private lateinit var diceImage : ImageView
+    private lateinit var diceImage2 : ImageView
+    private lateinit var linearView: View
+    private lateinit var textTotal: TextView
+    private var total: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        diceImage = findViewById(R.id.dice_image)
+        diceImage = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
+        textTotal = findViewById(R.id.textView)
+
         linearView = findViewById(R.id.view)
         linearView.setOnClickListener {
+            total = 0
             rollDice(diceImage)
+            rollDice(diceImage2)
+            textTotal.text = total.toString()
         }
     }
 
@@ -34,18 +44,20 @@ class MainActivity : AppCompatActivity() {
      * Roll the dice and update the screen with the result.
      */
     private fun rollDice(diceImage: ImageView) {
-        diceImage.setImageResource(getRandomDiceImage())
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        var roll = dice.roll()
+        diceImage.setImageResource(getRandomDiceImage(roll))
+        total += roll
     }
 
     /**
      * Roll the dice and update the screen with the result.
      */
-    private fun getRandomDiceImage() : Int {
-        // Create new Dice object with 6 sides and roll it
-        val dice = Dice(6)
+    private fun getRandomDiceImage(num: Int) : Int {
 
         // Update the screen with the dice roll
-        return when (dice.roll()) {
+        return when (num) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
