41a47a4586081e7ddb2f0e19a6bab7869e2e10ff
skyme32 <marcosmejiasg@gmail.com>
2020-10-21 19:17:50 +0200

    update the xml dice and the random dice

--- app/src/main/java/com/skyme32/diceroller/Dice.kt
+++ app/src/main/java/com/skyme32/diceroller/Dice.kt
@@ -1,8 +1,11 @@
 package com.skyme32.diceroller
 
+import java.util.*
+
 class Dice(private val numSides: Int) {
 
     fun roll(): Int {
-        return (1..numSides).random()
+        return Random().nextInt(numSides) + 1
+        //return (1..numSides).random()
     }
 }
